b7e5183df3ae69b382d278a742ad7acb21c8dc87
unknown <nkululekokoena@gmail.com>
2022-10-08 16:10:09 +0200

    SpringBoot Crud

--- src/main/java/com/nkulu/SpringBootCrudAppApplication.java
+++ src/main/java/com/nkulu/SpringBootCrudAppApplication.java
@@ -2,13 +2,21 @@ package com.nkulu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootCrudAppApplication {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to AWS First Deployment...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrudAppApplication.class, args);
-		System.out.println("Connected to Server.");
+		//System.out.println("Connected to Server.");
 	}
 
 }
